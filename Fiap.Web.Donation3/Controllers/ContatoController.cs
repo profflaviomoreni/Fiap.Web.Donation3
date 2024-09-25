bd30b1cb5a5bc53a16357d680f9b77ae9d36799e
Flavio Moreni <PF0954@fiap.com.br>
2024-09-24 22:10:43 -0300

    aula01

---
+++ Fiap.Web.Donation3/Controllers/ContatoController.cs
@@ -0,0 +1,50 @@
+﻿using Fiap.Web.Donation3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fiap.Web.Donation3.Controllers
+{
+    public class ContatoController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            Console.WriteLine("Método Index");
+            //return Content("Flávio");
+            //return View();
+
+            //return RedirectToAction(nameof(Help));
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Index(ContatoModel contatoModel)
+        {
+            // INSERT INTO CONTATO VALUES ( contatoModel.nome, ... );
+
+
+            return View();
+        }
+
+
+
+        /*
+        [HttpPost]
+        public IActionResult Gravar(ContatoModel contatoModel) { 
+
+            // INSERT INTO CONTATO VALUES ( contatoModel.nome, ... );
+
+            return View();
+        }
+        */
+
+
+        [HttpGet]
+        public IActionResult Help()
+        {
+            Console.WriteLine("Método Help");
+            return View();
+        }
+
+    }
+}
